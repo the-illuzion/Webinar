d1e6d5b0d7183af5534073c95ea994483edc76ae
Vartika <vartikaagrawal38395@gmail.com>
2020-09-01 00:34:00 +0530

    one step..

--- src/app/landing-page/main.js
+++ src/app/landing-page/main.js
@@ -3,7 +3,7 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('1 Sep, 2020 00:35:00').getTime(),
+let countDown = new Date('1 Sep, 2020 00:50:00').getTime(),
     x = setInterval(function() {
 
       let now = new Date().getTime(),
